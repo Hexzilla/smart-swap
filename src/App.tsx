696cc33d550b5d5cec09089885e467b04c904b5d
rightvalue <bluebird6922@gmail.com>
2021-08-02 04:47:35 +0300

    add Swap component

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,38 @@
 import React from 'react';
 import { Router, Redirect, Route, Switch } from 'react-router-dom';
 import history from './routerHistory'
+import Swap from './views/Swap'
 import logo from './logo.svg';
 import './App.css';
 
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="/swap"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
+}
+
+
 function App() {
   return (
-    <Router history={history}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    <Router history={history}>      
+      <Switch>
+        <Route exact strict path="/" component={Home} />
+        <Route exact strict path="/swap" component={Swap} />
+      </Switch>
     </Router>
   );
 }
---
+++ src/views/Swap/index.tsx
@@ -0,0 +1,10 @@
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
+import { RouteComponentProps } from 'react-router-dom'
+
+export default function Swap({ history }: RouteComponentProps) {
+  return (
+    <div>
+      Swap
+    </div>
+  )
+}
