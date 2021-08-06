12cbc792ec96f048f0e2ccb60b81e44f659aa350
goldencat-tom <bluebird6922@gmail.com>
2021-08-05 17:44:30 -0700

    add Web3Provider

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import { Web3ReactProvider } from '@web3-react/core'
 import { HelmetProvider } from 'react-helmet-async'
 import { Provider } from 'react-redux'
+import { getLibrary } from 'utils/web3React'
 import store from 'state'
 
 const Providers: React.FC = ({ children }) => {
   return (
-    <Provider store={store}>
-      <HelmetProvider>
-        {children}
-      </HelmetProvider>
-    </Provider>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Provider store={store}>
+        <HelmetProvider>
+          {children}
+        </HelmetProvider>
+      </Provider>
+    </Web3ReactProvider>
   )
 }
 
--- src/utils/web3React.ts
+++ src/utils/web3React.ts
@@ -1,6 +1,7 @@
 import { InjectedConnector } from '@web3-react/injected-connector'
 import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
 import { BscConnector } from '@binance-chain/bsc-connector'
+import { ethers } from 'ethers'
 import getNodeUrl from './getRpcUrl'
 
 const POLLING_INTERVAL = 12000
@@ -23,3 +24,10 @@ export const connectorsByName = {
   walletconnect: walletconnect,
   bsc: bscConnector,
 }
+
+export const getLibrary = (provider): ethers.providers.Web3Provider => {
+  const library = new ethers.providers.Web3Provider(provider)
+  library.pollingInterval = POLLING_INTERVAL
+  return library
+}
+
