850a3ef3eb0211eccee97f4c4b4757c5e48e86f3
goldencat-tom <bluebird6922@gmail.com>
2021-08-06 17:11:58 -0700

    check account in swap page

--- src/views/Swap/index.tsx
+++ src/views/Swap/index.tsx
@@ -3,12 +3,18 @@ import { RouteComponentProps } from 'react-router-dom'
 import UnlockButton from 'components/UnlockButton'
 
 import useActiveWeb3React from '../../hooks/useActiveWeb3React'
+import state from 'state'
+
 export default function Swap({ history }: RouteComponentProps) {
   const { account } = useActiveWeb3React()
   return (
     <div>
       Token Swap
-      <UnlockButton></UnlockButton>
+      { !account ? (
+        <UnlockButton width="100%" />
+      ) : (
+        <div>Swap</div>
+      )}
     </div>
   )
 }
