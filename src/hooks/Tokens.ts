f294f31ae8cd3b09d73ed961426bb9fa07d236a3
goldencat-tom <bluebird6922@gmail.com>
2021-08-12 04:50:16 -0700

    add hook Token

---
+++ src/hooks/Tokens.ts
@@ -0,0 +1,6 @@
+/* eslint-disable no-param-reassign */
+import { Currency, ETHER } from 'sdk'
+
+export function useCurrency(currencyId: string | undefined): Currency | null | undefined {
+  return ETHER
+}
